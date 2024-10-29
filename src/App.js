8f4da0a0e3db5be91a23ce390d9e42e180a578d1
Yashar Zolmajdi <yashar.zolmajdi@gmail.com>
2024-10-29 13:56:20 +1100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Keycloak from "keycloak-js";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [keycloakInstance, setKeycloakInstance] = useState();
+  const [isAuthenticated, setIsAuthenticated] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const keycloak = new Keycloak({
+        url: "https://dev.id.q-ctrl.com/auth",
+        realm: "q-ctrl",
+        clientId: "black-opal-website", //"black-opal-web-client",
+      });
+
+      setKeycloakInstance(keycloak);
+
+      try {
+        const authenticated = await keycloak.init({ onLoad: "login-required" });
+        console.log(
+          `User is ${authenticated ? "authenticated" : "not authenticated"}`
+        );
+        setIsAuthenticated(authenticated);
+      } catch (error) {
+        console.error("Failed to initialize adapter:", error);
+      }
+    })();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isAuthenticated === false && (
+        <>
+          <div>
+            <button
+              onClick={() => {
+                keycloakInstance.login();
+              }}
+            >
+              Login
+            </button>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                keycloakInstance.register();
+              }}
+            >
+              Register
+            </button>
+          </div>
+        </>
+      )}
+      {isAuthenticated === true && (
+        <>
+          <pre>{JSON.stringify(keycloakInstance.userInfo, null, 2)}</pre>
+          <div>
+            <button
+              onClick={() => {
+                keycloakInstance.login({ action: "CONFIGURE_TOTP" });
+              }}
+            >
+              Setup OTP
+            </button>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                keycloakInstance.logout();
+              }}
+            >
+              Logout
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
